83e434b63271314c4f3ecad040b748e97dfd2106
kosperera <therealkosala@gmail.com>
2024-07-17 00:14:17 -0400

    feat: lambda to write requests to s3 bucket

--- src/main/java/io/skol/resthooks/lambda/ReceiveEventHandler.java
+++ src/main/java/io/skol/resthooks/lambda/ReceiveEventHandler.java
@@ -5,13 +5,21 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.amazonaws.services.lambda.runtime.logging.LogLevel;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.inject.Inject;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.HttpStatusCode;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 public class ReceiveEventHandler
         implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+
+    @Inject
+    private S3Client s3;
 
     @Inject
     private ObjectMapper serializer;
@@ -21,14 +29,23 @@ public class ReceiveEventHandler
             final Context context) {
 
         String content = input.getBody();
-        try {
-            final Event im = this.serializer.readValue(content, Event.class);
-            final Ack res =
-                    new Ack(UUID.fromString(context.getAwsRequestId()), im);
+        String reqId = context.getAwsRequestId();
 
-            return OK(res);
-        } catch (Exception e) {
-            return BadRequest(e);
+        try {
+            // final Event im = this.serializer.readValue(content, Event.class);
+
+            // final S3Client s3 = S3Client.builder().useArnRegion(true).build();
+            final PutObjectRequest request =
+                    PutObjectRequest.builder().bucket("sb-s3-kitchensync-receiver-01")
+                            .key("file-name-" + reqId + ".json").build();
+
+            // s3.putObject(request, RequestBody.fromString(content));
+            final Ack ack = new Ack(UUID.fromString(reqId), content);
+
+            return OK(ack);
+        } catch (Exception ex) {
+            context.getLogger().log(ex.toString(), LogLevel.ERROR);
+            return BadRequest(ex);
         }
     }
 
